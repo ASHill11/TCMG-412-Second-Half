9a871600daa6a533894d227e43668c2fcdf7c79a
Aidan Hill <123518297+ASHill11@users.noreply.github.com>
2023-02-20 18:23:09 -0600

    Cailin's changes

--- API.py
+++ API.py
@@ -35,8 +35,32 @@ All returned values should be JSON
 """
 
 # ALL IMPORTS GO HERE
-from flask import flask
-from flask_restful import Resource, Api, reqparse
+from flask import Flask, jsonify
+import hashlib
+
+#Not sure what this is fore but, I think it defines the same of the app that we want to use for this
+app = Flask(__name__)
+
+#Hex uri
+@app.route('/md5/<string:input_string>')
+def md5(input_string):
+    md5_hash = hashlib.md5(input_string.encode()).hexdigest()
+    result = {'input': input_string, 'output': md5_hash}
+    return jsonify(result)
+
+#Factorial URI
+@app.route('/factorial/<int:n>')
+def factorial(n):
+    if n < 0:
+        return jsonify({'input': n, 'output': 'Error: input should be a positive integer'})
+    elif n == 0:
+        return jsonify({'input': n, 'output': 1})
+    else:
+        fact = 1
+        for i in range(1, n+1):
+            fact *= i
+        result = {'input': n, 'output': fact}
+        return jsonify(result)
 
 
 class Users(Resource):
