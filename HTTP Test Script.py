c4edca839c1c827c397d6aa27cf0c783a8f38885
Aidan <ASHill11@me.com>
2023-03-06 11:06:53 -0600

    Adding more tests, not all present tests are functional

--- HTTP Test Script.py
+++ HTTP Test Script.py
@@ -6,7 +6,17 @@ import requests
 counter = 0
 
 
-def check_api_status(url):
+def check_home_status(url):
+    response = requests.get(url)
+    return response.status_code
+
+
+def deck_md5(url):
+    response = requests.get(url)
+    return response.status_code
+
+
+def check_factorial_negative(url):
     response = requests.get(url)
     return response.status_code
 
@@ -14,8 +24,21 @@ def check_api_status(url):
 # Must include the http://
 # Example URL: http://127.0.0.1:8000/
 api_url = input('Enter URL: ')
-response_code = check_api_status(api_url)
-print(f"API response code: {response_code}")
+
+# ALL TESTS GO HERE
+# TEST 1
+response_code = check_home_status(api_url)
+print(f"/ response code: {response_code}")
 
 if response_code == 200:
     counter = counter + 1
+
+# TEST 2
+fact_neg = api_url + '/fibonacci/-1'
+response_code = check_factorial_negative(fact_neg)
+print(f"/ response code: {response_code}")
+
+if response_code == 200:
+    counter = counter + 1
+
+print('{}/2 checks passed'.format(counter))
