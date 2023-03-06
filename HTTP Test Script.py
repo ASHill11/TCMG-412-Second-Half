0eac8fe32c94f44f0418cde3985bcdfb32d82cc5
Aidan <ASHill11@me.com>
2023-03-06 10:53:38 -0600

    Basic check passes, adding a counter system now.

--- HTTP Test Script.py
+++ HTTP Test Script.py
@@ -2,13 +2,20 @@
 # pip3 install requests
 import requests
 
+# This variable will count the total number of checks passed
+counter = 0
+
 
 def check_api_status(url):
     response = requests.get(url)
     return response.status_code
 
 
-# Example usage
-api_url = input('Enter URL')
+# Must include the http://
+# Example URL: http://127.0.0.1:8000/
+api_url = input('Enter URL: ')
 response_code = check_api_status(api_url)
 print(f"API response code: {response_code}")
+
+if response_code == 200:
+    counter = counter + 1
