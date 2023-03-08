8053ed5bb766a8bf2c4ac0ef77df31bf180b24e4
Aidan <ASHill11@me.com>
2023-03-07 20:03:20 -0600

    Beginning of rewrite. Going to streamline code using for loops

--- HTTP Test Script.py
+++ HTTP Test Script.py
@@ -5,21 +5,15 @@ import requests
 
 def main():
     # This variable will count the total number of checks passed
-    counter = 0
-
-    def check_home_status(url):
-        response = requests.get(url)
-        return response.status_code
-
-    def check_md5(url):
-        response = requests.get(url)
-        return response.status_code
-
-    def check_factorial_negative(url):
-        response = requests.get(url)
-        return response.status_code
-
+    passed = 0
+    tested = 0
     api_url = 'http://localhost:8000/'
+
+    def http_status_check(url):
+        response = requests.get(url)
+        return response.status_code
+
+
 
     # ALL TESTS GO HERE
     # TEST 1
@@ -29,58 +23,10 @@ def main():
     if response_code == 200:
         counter = counter + 1
 
-    # TEST 2
-    md5 = api_url + '/md5/'
-    response_code = check_md5(md5)
-    print(f"/md5/ response code: {response_code}")
-
-    if response_code == 200:
-        counter = counter + 1
-
-    # TEST 3
-    md5_0 = api_url + '/md5/0'
-    response_code = check_md5(md5_0)
-    print(f"/md5/0 response code: {response_code}")
-
-    if response_code == 200:
-        counter = counter + 1
-
-    # TEST 4
-    fact_neg = api_url + '/factorial/-1'
-    response_code = check_factorial_negative(fact_neg)
-    print(f"/factorial/-1 response code: {response_code}")
-
-    if response_code == 200:
-        counter = counter + 1
-
-    # TEST 5
-    fact_0 = api_url + '/factorial/0'
-    response_code = check_factorial_negative(fact_0)
-    print(f"/factorial/0 response code: {response_code}")
-
-    if response_code == 200:
-        counter = counter + 1
-
-    # TEST 6
-    fact_1 = api_url + '/factorial/1'
-    response_code = check_factorial_negative(fact_1)
-    print(f"/factorial/1 response code: {response_code}")
-
-    if response_code == 200:
-        counter = counter + 1
-
-    # TEST 7
-    fact_999 = api_url + '/factorial/999'
-    response_code = check_factorial_negative(fact_999)
-    print(f"/factorial/999 response code: {response_code}")
-
-    if response_code == 200:
-        counter = counter + 1
 
     # Test checker
-    print('{}/7 checks passed'.format(counter))
-    total_tests = 6
-    if counter == total_tests:
+    print('{}/{} checks passed'.format(passed, tested))
+    if passed == tested:
         return 0
     else:
         return 1
