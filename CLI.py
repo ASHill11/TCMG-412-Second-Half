6aa16516cc903b9d5e9825a0567993614acaacc9
Aidan Hill <123518297+ASHill11@users.noreply.github.com>
2023-05-02 12:43:45 -0500

    Removed match/case syntax to better comply with python3 3.9 syntax

--- CLI.py
+++ CLI.py
@@ -1,5 +1,3 @@
-url = 'http://34.16.146.25:80/'
-
 print("welcome to the g0-api service terminal")
 print("for more information enter \"help\" or flag \"--help\" following a command")
 print("\"exit\" to close terminal")
@@ -9,7 +7,7 @@ def help_text():
     print("this terminal allows one to interact with the remote api service")
     print("using this api entails one of four http methods, an endpoint, a payload, and a JSON response")
     print("the basic structure of commands is as follows:")
-    print("[HTTP METHOD] /[ENDPOINT]/ ")
+    print("[HTTP METHOD] [ENDPOINT] [ARGUMENTS]")
     print("allowed HTTP methods: GET, PUT, POST, DELETE")
     print("not all HTTP methods are allowed for every endpoint")
     print("endpoints: md5, factorial, fibonacci, is-prime, keyval")
@@ -17,15 +15,47 @@ def help_text():
     print("\"exit\" to exit the terminal")
 
 
-while True:
-    terminal = input("$ ").lower()
-    inputs = terminal.split()
-    print(inputs)
+def method_help(method):
+    print("different http methods do different things to each endpoint")
+    print("not every method is allowed for every endpoint, a 405 error will be raised if it is not")
+    print("contextual help for the method you specified:")
 
-    if terminal =="help":
-        help_text()
-    if terminal == "exit":
-        exit()
-    case "get":
-        pass
-    print(f"unknown command: {terminal}")
+    if method == 'get':
+        print("The get method is allowed for all endpoints")
+        print("for specific usage, type \"get [endpoint] --help\"")
+    elif method == 'put':
+        print("The put method is only allowed for /keyval/")
+        print("for specific usage, type \"put keyval --help\"")
+    elif method == 'post':
+        print("The post method is only allowed for /keyval/")
+        print("for specific usage, type \"post keyval --help\"")
+    elif method == 'delete':
+        print("The delete method is only allowed for /keyval/")
+        print("for specific usage, type \"delete keyval --help\"")
+
+
+def main():
+    while True:
+        terminal = input("$ ").lower()
+
+        if terminal == "help":
+            help_text()
+        if terminal == "exit":
+            exit()
+
+        inputs = terminal.split()
+
+        url = 'http://34.16.146.25:80'
+        methods = ["get", "put", "post", "delete"]
+        endpoints = ["md5", "factorial", "fibonacci", "is-prime", "keyval"]
+
+        if inputs[0] not in methods:
+            print(f"unknown command: {terminal}")
+        else:
+            if inputs[1] == "--help":
+                method_help(inputs[0])
+            else:
+                print(f'{inputs[0]} success')
+
+
+main()
