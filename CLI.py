dffd72c2bb418c798de68080aaf1bb2a96318724
Aidan Hill <123518297+ASHill11@users.noreply.github.com>
2023-05-02 11:58:32 -0500

    Fleshing out CLI

--- CLI.py
+++ CLI.py
@@ -6,8 +6,12 @@ print("\"exit\" to close terminal")
 def help_text():
     print("this terminal allows one to interact with the remote api service")
     print("using this api entails one of four http methods, an endpoint, a payload, and a JSON response")
-    print("the structure of commands is as follows:")
-    print("[HTTP METHOD] /[ENDPOINT] ")
+    print("the basic structure of commands is as follows:")
+    print("[HTTP METHOD] /[ENDPOINT]/ ")
+    print("allowed HTTP methods: GET, PUT, POST, DELETE")
+    print("not all HTTP methods are allowed for every endpoint")
+    print("endpoints: md5, factorial, fibonacci, is-prime, keyval")
+    print("for more detailed instructions, enter a command followed by \"--help\" ")
 
 
 while True:
