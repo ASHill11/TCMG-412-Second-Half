7a0830eb4eb52e6a5a6bdc63709b6d7e80557e4d
Aidan Hill <123518297+ASHill11@users.noreply.github.com>
2023-05-02 11:53:49 -0500

    Fleshing out CLI

--- CLI.py
+++ CLI.py
@@ -1,15 +1,19 @@
 print("welcome to the g0-api service terminal")
 print("for more information enter \"help\" or flag \"--help\" following a command")
+print("\"exit\" to close terminal")
 
 
 def help_text():
     print("this terminal allows one to interact with the remote api service")
-    print("using this api entails one of four http commands, an endpoint, a payload, and a JSON response")
+    print("using this api entails one of four http methods, an endpoint, a payload, and a JSON response")
     print("the structure of commands is as follows:")
-    print
+    print("[HTTP METHOD] /[ENDPOINT] ")
+
 
 while True:
     terminal = input("$ ")
     match terminal:
         case "help":
             print("help selected")
+        case "exit":
+            exit()
